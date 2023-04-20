d6df0d819df972d82907e3070c601a3a411500f5
Lucas Bonacorsi <lucasbonacorsi@hotmail.com>
2023-04-19 23:25:01 -0300

    feat: dist folder deleted

--- dist/stories/components/MyLabel.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import { StoryObj } from "@storybook/react";
-declare const meta: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, import("../../components/MyLabel").MyLabelProps>;
-type Story = StoryObj<typeof meta>;
-export default meta;
-export declare const Basic: Story;
-export declare const AllCaps: Story;
-export declare const Secondary: Story;
-export declare const Tertiary: Story;
-export declare const CustomFontColor: Story;
